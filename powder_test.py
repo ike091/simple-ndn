dc5937d7b309312bb08ef1914debce791cfbce27
ike091 <ike091@gmail.com>
2020-05-19 11:32:02 -0600

    update powder_test.py, rename files for clarity

--- powder_test.py
+++ powder_test.py
@@ -8,23 +8,27 @@ import geni.rspec.emulab as elab
 
 
 class GLOBALS(object):
+    """useful constant values for setting up a powder experiment"""
     SITE_URN = "urn:publicid:IDN+emulab.net+authority+cm"
+    # standard Ubuntu release
     UBUNTU18_IMG = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD"
-    PNODE_TYPE = "d840"
+    PNODE_D740 = "d740"  # 24 cores, 192 GB RAM
+    PNODE_D840 = "d840"  # 64 cores, 768 GB RAM
 
 
-def mkVM(name, image, cores=4, ram=4096):
+def mkVM(name, image, cores=4, ram=4):
     """Creates a VM with the specified parameters"""
     node = request.XenVM(name)
     node.disk_image = image
     node.cores = cores
-    node.ram = ram
+    node.ram = ram * 1024
     node.exclusive = True
     node.routable_control_ip = True
     node.InstantiateOn('pnode')
     return node
 
 
+# begin creating request
 pc = portal.Context()
 
 request = pc.makeRequestRSpec()
@@ -32,30 +36,19 @@ request = pc.makeRequestRSpec()
 
 # Declare dedicated VM host
 pnode = request.RawPC('pnode')
-pnode.hardware_type = GLOBALS.PNODE_TYPE
-
+pnode.hardware_type = GLOBALS.PNODE_D740
 
 # configure VMs and links here:
-node1 = mkVM('node1', GLOBALS.UBUNTU18_IMG, cores=2, ram=2048)
-node2 = mkVM('node2', GLOBALS.UBUNTU18_IMG, cores=2, ram=2048)
-node3 = mkVM('node3', GLOBALS.UBUNTU18_IMG, cores=2, ram=2048)
-node4 = mkVM('node4', GLOBALS.UBUNTU18_IMG, cores=2, ram=2048)
+node1 = mkVM('node1', GLOBALS.UBUNTU18_IMG, cores=4, ram=8)
+node2 = mkVM('node2', GLOBALS.UBUNTU18_IMG, cores=4, ram=8)
+node3 = mkVM('node3', GLOBALS.UBUNTU18_IMG, cores=4, ram=8)
+node4 = mkVM('node4', GLOBALS.UBUNTU18_IMG, cores=4, ram=8)
 
-# Add links between all four nodes
+# Add links between nodes (form a "circle" of connectivity)
 link1 = request.Link(members=[node1, node2])
 link2 = request.Link(members=[node2, node3])
 link3 = request.Link(members=[node3, node4])
 link4 = request.Link(members=[node4, node1])
 
-
-# link = request.Link("link")
-# link.trivial_ok = True
-# link.addNode(node1)
-# link.addNode(node2)
-# link.addNode(node3)
-# link.addNode(node4)
-
-# link1 = request.Link(members=[node1, node2, node3, node4])
-
-# print request
+# output request
 pc.printRequestRSpec(request)
