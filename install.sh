9e4945f01aff44d00f24ae2cd01b9550c90b2245
ike091 <ike091@gmail.com>
2020-05-20 13:55:14 -0600

    add install.sh and update profile to run script

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# this script installs the NDN forwarding daemon
+
+sudo apt-get install software-properties-common -y
+sudo add-apt-repository ppa:named-data/ppa -y
+sudo apt-get update
+
+sudo apt-get install nfd -y
